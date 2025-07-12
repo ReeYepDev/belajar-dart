a28ec8d7dbea4d1a07a6cbd09bd991eaad36d59b
ReeYep || Beginner Programmer. <muhammadalfarizkian@gmail.com>
2025-07-12 18:16:12 +0700

    Create menampilkan-nama-dan-asal.dart

---
+++ menampilkan-nama-dan-asal.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Mengindentifikasikan nama mahasiswa dan tempat tinggalnya berdasarkan indeks array
+  var mahasiswa = <String>['Budi', 'Ari', 'Arkan', 'Zafir'];
+  var tempatTinggal = <String>['Jawa Tengah', 'Bandung', 'Subulussalam', 'Samarinda'];
+
+  // Cek apakah jumlah data mahasiswa dan tempatTinggal sama banyaknya
+  if (mahasiswa.length == tempatTinggal.length) {
+    // Menampilkan setiap data sesuai dengan teks
+    for (int i = 0; i < mahasiswa.length; i++) {
+      print('${mahasiswa[i]} berasal dari ${tempatTinggal[i]}.');
+    }
+  } else {
+    print("Data tidak cukup");
+  }
+}
+
+// Membuat teks asal
+String asal(String nama, String tempat) {
+  return '$nama berasal dari $tempat';
+}
